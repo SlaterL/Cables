fa8fb3557525470fe643520ae6a23bc6e79e3dde
Slater Lloyd <slater.lloyd@vivint.com>
2024-12-10 00:26:09 -0700

    publishing

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package cables
 import (
 	pb "cables/generated"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -11,50 +12,100 @@ import (
 )
 
 type CablesClient struct {
-	client pb.CablesServiceClient
+	client         pb.CablesServiceClient
+	PublishChannel chan *pb.Message
+	quitCh         chan bool
+	handleFunc     func(*pb.Message) error
+	conn           *grpc.ClientConn
+	config         *CablesClientConfig
 }
 
-func NewClient(client pb.CablesServiceClient) *CablesClient {
-	return &CablesClient{
-		client: client,
+type CablesClientConfig struct {
+	ClientName    string   `json:"client_name,omitempty"`
+	ConsumerGroup string   `json:"consumer_group,omitempty"`
+	ConsumeTopics []string `json:"consume_topics,omitempty"`
+	CanConsume    bool     `json:"can_consume,omitempty"`
+}
+
+func NewClient(config *CablesClientConfig, handleFunc func(*pb.Message) error) *CablesClient {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
 	}
+	conn, err := grpc.NewClient("localhost:8082", opts...)
+	if err != nil {
+		log.Fatalf("Conn failure: %v", err)
+	}
+
+	return &CablesClient{
+		client:         pb.NewCablesServiceClient(conn),
+		PublishChannel: make(chan *pb.Message, 100),
+		quitCh:         make(chan bool),
+		handleFunc:     handleFunc,
+		conn:           conn,
+		config:         config,
+	}
+}
+
+func (c *CablesClient) Close() {
+	if c.conn != nil {
+		c.conn.Close()
+	}
+	close(c.quitCh)
+	close(c.PublishChannel)
+}
+
+func (c *CablesClient) Publish(message *pb.Message) error {
+	c.PublishChannel <- message
+	return nil
 }
 
 func (c *CablesClient) Hook() error {
 	opts := []grpc.CallOption{}
 	stream, err := c.client.Hook(context.Background(), opts...)
 	if err != nil {
-		return nil
+		return err
 	}
-	waitc := make(chan struct{})
+	configBytes, errConfig := json.Marshal(c.config)
+	if errConfig != nil {
+		return errConfig
+	}
+
+	stream.Send(&pb.Message{
+		Message: configBytes,
+		Qos:     1,
+	})
+	// Publish
+	go func() {
+		for message := range c.PublishChannel {
+			stream.Send(message)
+		}
+	}()
+
+	// Consume
 	go func() {
 		for {
 			in, err := stream.Recv()
 			if err == io.EOF {
 				// read done.
-				close(waitc)
+				c.Close()
 				return
 			}
 			if err != nil {
-				log.Fatalf("Failed to receive a note : %v", err)
+				log.Fatalf("Failed to receive a note: %v", err)
 			}
-			log.Printf("Got message %s", in.Message)
+			errHandle := c.handleFunc(in)
+			if errHandle != nil {
+				log.Fatalf("Error in message processing: %v", errHandle)
+			}
 		}
 	}()
 
-	for i := range 5 {
-		m := &pb.Message{
-			Qos:     1,
-			Message: []byte(fmt.Sprintf("Here is a message: %d", i)),
-		}
-		stream.Send(m)
-	}
+	<-c.quitCh
+	fmt.Println("Exiting Hook")
 	err = stream.CloseSend()
 	if err != nil {
 		return err
 	}
-	<-waitc
 
 	return nil
-
 }
--- cmd/cables/server.go
+++ cmd/cables/server.go
@@ -1,40 +1,95 @@
 package main
 
 import (
+	"cables"
 	pb "cables/generated"
+	"encoding/json"
 	"fmt"
 	"io"
 )
 
 type cablesServer struct {
 	pb.UnimplementedCablesServiceServer
+	consumers []*Consumer
+}
+
+type Consumer struct {
+	Name          string
+	ReturnStream  pb.CablesService_HookServer
+	ConsumerGroup string
+	Topics        []string
+}
+
+func (c *Consumer) Consume(message *pb.Message) error {
+	return c.ReturnStream.Send(message)
+}
+
+func (s *cablesServer) processClientConfig(config *cables.CablesClientConfig, stream pb.CablesService_HookServer) {
+	if config.CanConsume {
+		newConsumer := &Consumer{
+			Name:          config.ClientName,
+			ConsumerGroup: config.ConsumerGroup,
+			Topics:        config.ConsumeTopics,
+			ReturnStream:  stream,
+		}
+		s.consumers = append(s.consumers, newConsumer)
+	}
+}
+
+func (s *cablesServer) Cleanup() {
+	var aliveConsumers []*Consumer
+	for _, consumer := range s.consumers {
+		if consumer.ReturnStream.Context().Err() == nil {
+			aliveConsumers = append(aliveConsumers, consumer)
+		}
+	}
+	s.consumers = aliveConsumers
 }
 
 func (s *cablesServer) Hook(stream pb.CablesService_HookServer) error {
 	fmt.Println("Connection Established")
+	configMessage, errConfig := stream.Recv()
+	var config cables.CablesClientConfig
+	json.Unmarshal(configMessage.GetMessage(), &config)
+	s.processClientConfig(&config, stream)
+	if errConfig != nil {
+		return errConfig
+	}
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
+			// TODO: resolve client remove bug
+			s.Cleanup()
 			return nil
 		}
 		if err != nil {
 			return err
 		}
-		fmt.Println(string(in.Message))
-		if in.Qos > 0 {
-			retMessage := pb.Message{
-				Message: []byte("Ack"),
-				Qos:     0,
-			}
-			sendErr := stream.Send(&retMessage)
-			if sendErr != nil {
-				return sendErr
+		s.ProcessPublished(in)
+	}
+}
+
+func StringInSlice(s string, list []string) bool {
+	for _, item := range list {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *cablesServer) ProcessPublished(message *pb.Message) error {
+	for _, consumer := range s.consumers {
+		if StringInSlice(message.GetTopic(), consumer.Topics) {
+			errConsume := consumer.Consume(message)
+			if errConsume != nil {
+				return errConsume
 			}
 		}
 	}
+	return nil
 }
 
 func NewCablesServer() *cablesServer {
 	return &cablesServer{}
-
 }
